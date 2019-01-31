eae85841d1e307bb5dafa5b6e4cbe01efff5a888
pblakescit <46540019+pblakescit@users.noreply.github.com>
2019-01-31 07:51:17 -0500

    Create PromptVar.java

---
+++ PromptVar.java
@@ -0,0 +1,27 @@
+/**
+ * Write a description of class VarPrompt here.
+ *
+ * @author (your name)
+ * @date
+ */
+
+import java.util. Scanner; //required import for terminal IO
+
+public class VarPromptNotes
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in); //Required Line for terminal IO
+        
+        //Example of creating a string variable
+        
+        //Example of printing a reference instead of an explicit value
+        
+        //How to update a String value
+        
+        //How to prompt for a String
+        
+        //How to use a prompted value 
+        
+    }
+}
